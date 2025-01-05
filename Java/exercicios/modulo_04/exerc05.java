62f8a82eccdbb79a32f1ab9b0772536ef03b80b8
Paulo Henrique <phdsreis@gmail.com>
2025-01-05 11:57:25 -0300

    feat: first commit

---
+++ Java/exercicios/modulo_04/exerc05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exerc05 {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Valor de A: ");
+        a = sc.nextInt();
+        System.out.print("Valor de B: ");
+        b = sc.nextInt();
+        System.out.print("Valor de C: ");
+        c = sc.nextInt();
+        System.out.print("Valor de D: ");
+        d = sc.nextInt();
+        sc.close();
+        
+        int diferenca = (a * b - c * d);
+        System.out.println("Diferença: " + diferenca);
+    }
+}
---
+++ Java/exercicios/modulo_04/exerc08.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exerc08 {
+    public static void main(String[] args) {
+        double a, b, c;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Valores de A,B e C: ");
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
+        sc.close();
+
+        double areaTriangulo = a * c / 2;
+        double areaCirculo = 3.14159 * Math.pow(c, 2);
+        double areaTrapezio = (a + b) * c / 2;
+        double areaQuadrado = Math.pow(b, 2);
+        double areaRetangulo = a * b;
+
+        System.out.printf("Triângulo: %.3f%n", areaTriangulo);
+        System.out.printf("Círculo: %.3f%n", areaCirculo);
+        System.out.printf("Trapézio: %.3f%n", areaTrapezio);
+        System.out.printf("Quadrado: %.3f%n", areaQuadrado);
+        System.out.printf("Retângulo: %.3f%n", areaRetangulo);
+    }
+}
